511298d10683ddb524ae2b5cbed2cf815325437b
rage-proof <>
2021-04-11 00:47:37 +0200

    setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+from pymusig.version import __version__
+
+import pathlib
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
+
+
+setup(
+    name="pymusig",
+    version=__version__,
+    python_requires='>=3.5',
+    description="Implementation of the MuSig multisignature protocol for python",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    author="rage-proof",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    include_package_data=True,
+    packages=find_packages(exclude=["tests"]),
+    install_requires=['chacha20poly1305==0.0.3']
+)
